1e8011d5added20bc0321793c5128e5b29dbb096
Dilan Fernando <dilan.fernando@pickme.lk>
2024-02-13 23:28:56 +0200

    create context and update reference count

--- context/test.c
+++ context/test.c
@@ -6,23 +6,53 @@ int main(int argc, char* const argv[]) {
 
     cl_platform_id platform;
     cl_device_id device;
-    cl_uint err;
+    cl_int err;
+
+    cl_context context;
+    cl_uint ref_count;
 
     err = clGetPlatformIDs(1, &platform, NULL);
     if (err < 0) {
-        printf("Error: clGetPlatformIDs");
+        perror("Error: clGetPlatformIDs");
         exit(1);
     }
 
     err = clGetDeviceIDs(platform, CL_DEVICE_TYPE_ALL, 1, &device, NULL);
     if (err < 0) {
-        printf("Error: clGetDeviceIDs");
+        perror("Error: clGetDeviceIDs");
         exit(1);
     }
 
     char buf[128];
 
     clGetDeviceInfo(device, CL_DEVICE_NAME, 128, buf, NULL);
-    fprintf(stdout, "Device: %s ", buf);
+    fprintf(stdout, "Device: %s\n", buf);
+
+    context = clCreateContext(NULL, 1, &device, NULL, NULL, &err);
+    if (err < 0) {
+        perror("Error: clCreateContext");
+        exit(1);
+    }
+
+    err = clGetContextInfo(context, CL_CONTEXT_REFERENCE_COUNT, sizeof(ref_count), &ref_count, NULL);
+    if (err < 0) {
+        perror("Error: clGetContextInfo CL_CONTEXT_REFERENCE_COUNT");
+        exit(1);
+    }
+
+    printf("Initial reference count: %u\n", ref_count);
+
+    clRetainContext(context);
+    clGetContextInfo(context, CL_CONTEXT_REFERENCE_COUNT, sizeof(ref_count), &ref_count, NULL);
+
+    printf("Reference count: %u\n", ref_count);
+
+    clReleaseContext(context);
+    clGetContextInfo(context, CL_CONTEXT_REFERENCE_COUNT, sizeof(ref_count), &ref_count, NULL);
+
+    printf("Reference count: %u\n", ref_count);
+
+    clReleaseContext(context);
+    return 0;
 
 }
