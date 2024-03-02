2858dc809522d37eb83ff1b3a0d299be0e2cacbd
Rukshan Perera <2173530+KRVPerera@users.noreply.github.com>
2024-03-02 11:32:49 +0200

    fix: neighbour fix in other functions

--- src/pngloader.c
+++ src/pngloader.c
@@ -117,7 +117,7 @@ unsigned char *getNeighbourWindowWithMirroringUnsigned(Image *input, unsigned x,
             if (x1 < 0) {
                 x1 = abs(x1);
             } else if (x1 >= (int) input->width) {
-                x1 = input->width + (input->width - x1) - 2;
+                x1 = input->width + input->width - x1 - 2;
             }
 
             if (y1 < 0) {
@@ -143,13 +143,13 @@ float *getNeighbourWindowWithMirroring(Image *input, unsigned x, unsigned y, int
             if (x1 < 0) {
                 x1 = abs(x1);
             } else if (x1 >= (int) input->width) {
-                x1 = input->width + (input->width - x1) - 1;
+                x1 = input->width + input->width - x1 - 2;
             }
 
             if (y1 < 0) {
                 y1 = abs(y1);
             } else if (y1 >= (int) input->height) {
-                y1 = input->height + input->height - y1 - 1;
+                y1 = input->height + input->height - y1 - 2;
             }
 
             size_t index = 4 * input->width * y1 + 4 * x1;
@@ -169,12 +169,12 @@ float *getNeighboursZeroPaddingFloats(Image *input, unsigned x, unsigned y) {
             if (x1 < 0) {
                 x1 = abs(x1);
             } else if (x1 >= (int) input->width) {
-                x1 = input->width + (input->width - x1) - 1;
+                x1 = input->width + input->width - x1 - 2;
             }
             if (y1 < 0) {
                 y1 = abs(y1);
             } else if (y1 >= (int) input->height) {
-                y1 = input->height + input->height - y1 - 1;
+                y1 = input->height + input->height - y1 - 2;
             }
             size_t index = 4 * input->width * y1 + 4 * x1;
             neighbours[neighboursIndex] = input->image[index];
