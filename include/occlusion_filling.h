0074a8831b4cad2a4e0da993f64be2825003e5d0
Rukshan Perera <2173530+KRVPerera@users.noreply.github.com>
2024-04-28 10:35:26 +0300

    Add header comments

--- include/occlusion_filling.h
+++ include/occlusion_filling.h
@@ -1,7 +1,22 @@
 #ifndef MULTIPROCOPENCL_OCCULSION_FILLING_H
 #define MULTIPROCOPENCL_OCCULSION_FILLING_H
 
+#include <pngloader.h>
+
+/**
+ * Fill in occlusions in an image
+ * The caller is responsible for freeing the memory
+ * @param image
+ * @return
+ */
 Image *OcclusionFill(Image *image);
+
+/**
+ * Fill in occlusions in an image using OpenMP
+ * The caller is responsible for freeing the memory
+ * @param image
+ * @return
+ */
 Image *OcclusionFill_MT(Image *image);
 
-#endif //MULTIPROCOPENCL_OCCULSION_FILLING_H
+#endif// MULTIPROCOPENCL_OCCULSION_FILLING_H
